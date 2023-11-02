9c56425c3e838ceb9105cabc4379048f1047c45a
Luciano <lucianodacunha.net@gmail.com>
2023-11-02 17:47:23 -0300

    Curso concluido.

--- api/src/main/java/io/github/lucianodacunha/api/entity/Consulta.java
+++ api/src/main/java/io/github/lucianodacunha/api/entity/Consulta.java
@@ -35,6 +35,12 @@ public class Consulta {
     @Enumerated(EnumType.STRING)
     private MotivoCancelamento motivoCancelamento;
 
+    public Consulta(Long id, Medico medico, Paciente paciente, LocalDateTime data) {
+        this.medico = medico;
+        this.paciente = paciente;
+        this.data = data;
+    }
+
     public void cancelar(MotivoCancelamento motivo) {
         this.motivoCancelamento = motivo;
     }
